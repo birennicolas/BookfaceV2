fefa4a05af93610a5b5df77cc08e53fbb9e9d8a9
birennicolas <90913609+birennicolas@users.noreply.github.com>
2022-01-12 11:21:16 +0100

    CRUD

---
+++ crud_app/server/model/model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+    name : {
+        type : String,
+        required: true
+    },
+    firstname : {
+        type : String,
+        required: true
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true
+    },
+    birthdate : {
+        type: String,
+        required: true,
+        unique: true
+    },
+    gender : String,
+    status : String
+})
+
+const Userdb = mongoose.model('userdb', schema);
+
+module.exports = Userdb;
